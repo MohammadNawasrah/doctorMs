43733b11833b8eb678f88b41454b226c9c77b460
MohammadNawasrah <nawasrahmohammad2000@gmail.com>
2024-01-16 14:59:45 +0300

    add js files to bootstrap and socket install

--- resources/views/htmlCode.blade.php
+++ resources/views/htmlCode.blade.php
@@ -24,8 +24,6 @@
         </div>
     </div>
 </main>
-<script src="/js/bootstrap/popper.min.js"></script>
-<script src="/js/bootstrap/bootstrap.min.js"></script>
 <script>
     $(function() {
         Loader.removeLoadPage();
